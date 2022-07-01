68cc1339c74f21af61ae77b7598c5618338e4354
mkrupal09 <mkrupal09@gmail.com>
2022-07-01 19:17:26 +0530

    added image pic
    fixed image picker

--- app/src/main/java/com/example/mycomposecookbook/screen/ImagePickerActivity.kt
+++ app/src/main/java/com/example/mycomposecookbook/screen/ImagePickerActivity.kt
@@ -1,6 +1,11 @@
 package com.example.mycomposecookbook.screen
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class ImagePickerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //fixed
+    }
 }
